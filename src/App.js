3d09bf3b45c91d7e7c0335756ef9f9ab32b7b6d4
ddujeong <qoddl159@nate.com>
2025-11-04 17:52:53 +0900

    오늘의 운세 기능 예시

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Fortune from "./pages/Fortune.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/fortune" element={<Fortune />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
