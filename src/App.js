8e4bb9364fca9cbc90713eff078b599186f5e5e2
ddujeong <qoddl159@nate.com>
2025-11-05 14:39:32 +0900

    운세, 심리테스트 테스트용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Fortune from "./pages/Fortune.js";
+import Fortune from "./component/Fortune.js";
+import DailyTest from "./component/DailyTest.js";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/fortune" element={<Fortune />}></Route>
+        <Route path="/daily" element={<DailyTest />}></Route>
       </Routes>
     </div>
   );
