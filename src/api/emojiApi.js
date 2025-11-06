cae510ead21cdc3ecff47407a065d523fd01c0a9
Kukyeon <rnrdus2000@naver.com>
2025-11-06 12:39:10 +0900

    이모지 수정

--- src/api/emojiApi.js
+++ src/api/emojiApi.js
@@ -1,28 +1,28 @@
 import api from "./axiosConfig";
 
 export const emojiList = [
-  { id: 1, name: "heart", image: "/emojis/heart.png" },
-  { id: 2, name: "love", image: "/emojis/love.png" },
-  { id: 3, name: "happy", image: "/emojis/happy.png" },
-  { id: 4, name: "calm", image: "/emojis/calm.png" },
-  { id: 5, name: "laugh", image: "/emojis/laugh.png" },
-  { id: 6, name: "surprised", image: "/emojis/surprised.png" },
-  { id: 7, name: "joy", image: "/emojis/joy.png" },
-  { id: 8, name: "down", image: "/emojis/down.png" },
-  { id: 9, name: "confused", image: "/emojis/confused.png" },
-  { id: 10, name: "sad", image: "/emojis/sad.png" },
-  { id: 11, name: "dizzy", image: "/emojis/dizzy.png" },
-  { id: 12, name: "cry", image: "/emojis/cry.png" },
-  { id: 13, name: "gasp", image: "/emojis/gasp.png" },
-  { id: 14, name: "sick", image: "/emojis/sick.png" },
-  { id: 15, name: "anger", image: "/emojis/anger.png" },
+  { id: 1, type: "heart", image: "/emojis/heart.png" },
+  { id: 2, type: "love", image: "/emojis/love.png" },
+  { id: 3, type: "happy", image: "/emojis/happy.png" },
+  { id: 4, type: "calm", image: "/emojis/calm.png" },
+  { id: 5, type: "laugh", image: "/emojis/laugh.png" },
+  { id: 6, type: "surprised", image: "/emojis/surprised.png" },
+  { id: 7, type: "joy", image: "/emojis/joy.png" },
+  { id: 8, type: "down", image: "/emojis/down.png" },
+  { id: 9, type: "confused", image: "/emojis/confused.png" },
+  { id: 10, type: "sad", image: "/emojis/sad.png" },
+  { id: 11, type: "dizzy", image: "/emojis/dizzy.png" },
+  { id: 12, type: "cry", image: "/emojis/cry.png" },
+  { id: 13, type: "gasp", image: "/emojis/gasp.png" },
+  { id: 14, type: "sick", image: "/emojis/sick.png" },
+  { id: 15, type: "anger", image: "/emojis/anger.png" },
 ];
 
 //  게시글 이모지 토글
 export const toggleBoardEmoji = async (boardId, data) => {
   return await api.post(`/emoji/toggle`, {
     boardId,
-    accountId: data.accountId, // 필요 시 전달
+
     type: data.type,
     imageUrl: data.imageUrl,
   });
@@ -32,7 +32,7 @@ export const toggleBoardEmoji = async (boardId, data) => {
 export const toggleCommentEmoji = async (commentId, data) => {
   return await api.post(`/emoji/toggle`, {
     commentId,
-    accountId: data.accountId,
+
     type: data.type,
     imageUrl: data.imageUrl,
   });
