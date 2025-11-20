1ed442bbeca19a178dd5c25a5902d149d450e8e5
김윤섭 <dbstjq24@naver.com>
2025-11-20 12:19:14 +0900

    도메인 등록 접근

--- src/api/socialAuth.js
+++ src/api/socialAuth.js
@@ -1,18 +1,18 @@
 const KAKAO_REST_API_KEY = "d032aea47f7cde0d9d176389f15a4053";
-const KAKAO_REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
+const KAKAO_REDIRECT_URI = "https://mindmate.co.kr/auth/kakao/callback";
 const KAKAO_DELETE_REDIRECT_URI =
-  "http://localhost:3000/auth/kakao/delete-callback";
+  "https://mindmate.co.kr/auth/kakao/delete-callback";
 
 const NAVER_CLIENT_ID = "ca5VSclizDaIGPg981EJ";
-const NAVER_REDIRECT_URI = "http://localhost:3000/auth/naver/callback";
+const NAVER_REDIRECT_URI = "https://mindmate.co.kr/auth/naver/callback";
 const NAVER_DELETE_REDIRECT_URI =
-  "http://localhost:3000/auth/naver/delete-callback";
+  "https://mindmate.co.kr/auth/naver/delete-callback";
 
 const GOOGLE_CLIENT_ID =
   "866215417194-hsrr4k4h24rku7ng7le4rvnq8s9dqviv.apps.googleusercontent.com";
-const GOOGLE_REDIRECT_URI = "http://localhost:3000/auth/google/callback";
+const GOOGLE_REDIRECT_URI = "https://mindmate.co.kr/auth/google/callback";
 const GOOGLE_DELETE_REDIRECT_URI =
-  "http://localhost:3000/auth/google/delete-callback";
+  "https://mindmate.co.kr/auth/google/delete-callback";
 const GOOGLE_SCOPE = "openid email";
 
 // 공통 state 생성 함수
